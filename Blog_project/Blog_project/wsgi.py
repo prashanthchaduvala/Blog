012f7e0f0455219b3523c840fd143516e4a0113e
prashanthch <54631863+prashanthchaduvala@users.noreply.github.com>
2019-11-14 22:05:44 +0530

    Update wsgi.py

--- Blog_project/Blog_project/wsgi.py
+++ Blog_project/Blog_project/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 sys.path.append('Users\Prashanth\Documents\GitHub\Blog\Blog_project')
 
 from django.core.wsgi import get_wsgi_application
